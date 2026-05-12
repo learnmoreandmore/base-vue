742fd0fca861279f007451f95b73f86d32597cd9
ck <ck66da@163.com>
2026-05-12 21:24:02 +0800

    fixbug(ui): 更改引入element-plus的css样式的方式，解决函数式API按需样式不会随脚本import注入的问题

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import ElLoading from 'element-plus/es/components/loading/index.mjs'
 import 'element-plus/es/components/loading/style/css.mjs'
+/** Message 为函数式 API，按需样式不会随脚本 import 注入，需单独引入 */
+import 'element-plus/es/components/message/style/css.mjs'
 import App from './App.vue'
 import router from './router'
 import './styles/index.css'
